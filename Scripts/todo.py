e38dfa56d3a742405f815198ed7cec75be9cc26e
Yasir <yasirkaraman8@gmail.com>
2024-01-20 23:35:12 +0300

    Day 5

--- Scripts/todo.py
+++ Scripts/todo.py
@@ -2,6 +2,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import keyboard
 import json
+import datetime
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -77,6 +78,8 @@ class Ui_MainWindow(object):
 "font-weight: 400;\n"
 "line-height: normal;")
         self.zaman_dateTimeEdit.setObjectName("zaman_dateTimeEdit")
+        bugun_zaman = QtCore.QDateTime.currentDateTime()
+        self.zaman_dateTimeEdit.setDateTime(bugun_zaman)
         self.label_3 = QtWidgets.QLabel(self.widget)
         self.label_3.setGeometry(QtCore.QRect(1240, 567, 131, 51))
         self.label_3.setStyleSheet("QLabel{\n"
@@ -113,61 +116,32 @@ class Ui_MainWindow(object):
 "}")
         self.delete_button.setObjectName("delete_button")
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.widget)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(90, 260, 631, 661))
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(89, 258, 629, 657))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.frame = QtWidgets.QFrame(self.verticalLayoutWidget_2)
-        self.frame.setMinimumSize(QtCore.QSize(624, 88))
-        self.frame.setMaximumSize(QtCore.QSize(624, 88))
-        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame.setObjectName("frame")
-        self.label_4 = QtWidgets.QLabel(self.frame)
-        self.label_4.setGeometry(QtCore.QRect(40, 10, 521, 31))
-        self.label_4.setStyleSheet("QLabel{\n"
-"color: #000;\n"
-"font-family: Arial;\n"
-"font-size: 22px;\n"
-"font-style: normal;\n"
-"font-weight: 700;\n"
-"line-height: normal;\n"
-"}")
-        self.label_4.setObjectName("label_4")
-        self.checkBox = QtWidgets.QCheckBox(self.frame)
-        self.checkBox.setGeometry(QtCore.QRect(10, 30, 16, 21))
-        self.checkBox.setText("")
-        self.checkBox.setObjectName("checkBox")
-        self.label_5 = QtWidgets.QLabel(self.frame)
-        self.label_5.setGeometry(QtCore.QRect(40, 40, 491, 31))
-        self.label_5.setStyleSheet("QLabel{\n"
-"color: #000;\n"
-"font-family: Arial;\n"
-"font-size: 15px;\n"
-"font-style: normal;\n"
-"font-weight: 400;\n"
-"line-height: normal;\n"
-"}")
-        self.label_5.setObjectName("label_5")
-        self.pushButton_3 = QtWidgets.QPushButton(self.frame)
-        self.pushButton_3.setGeometry(QtCore.QRect(540, 30, 71, 41))
-        self.pushButton_3.setStyleSheet("QPushButton{\n"
-"color: #FFF;\n"
-"text-align: center;\n"
-"font: 63 10pt \"Segoe UI\";\n"
-"font-weight: 700;\n"
-"background-color:rgb(2, 99, 255);\n"
-"}")
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.verticalLayout_2.addWidget(self.frame)
-        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.verticalLayout_2.addItem(spacerItem)
-        MainWindow.setCentralWidget(self.centralwidget)
 
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.scrollArea = QtWidgets.QScrollArea(self.verticalLayoutWidget_2)
+        self.scrollArea.setMinimumSize(QtCore.QSize(670, 689))
+        self.scrollArea.setMaximumSize(QtCore.QSize(624, 689))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 668, 687))
+        self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
+
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents_2)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setAlignment(QtCore.Qt.AlignTop)  # Frameleri üst tarafa hizala
+        self.verticalLayout.setSpacing(0)  # Aralıkları sıfıra ayarla
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents_2)
+        self.verticalLayout_2.addWidget(self.scrollArea)
+        MainWindow.setCentralWidget(self.centralwidget)
+        
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-
+        self.todoList()
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
@@ -178,9 +152,6 @@ class Ui_MainWindow(object):
         self.label_3.setText(_translate("MainWindow", "Date"))
         self.add_button.setText(_translate("MainWindow", "Add"))
         self.delete_button.setText(_translate("MainWindow", "Delete"))
-        self.label_4.setText(_translate("MainWindow", "Title"))
-        self.label_5.setText(_translate("MainWindow", "Description"))
-        self.pushButton_3.setText(_translate("MainWindow", "More"))
     def closeDa(self):
         MainWindow.destroy(True,True)
 
@@ -192,14 +163,98 @@ class Ui_MainWindow(object):
         date = self.zaman_dateTimeEdit.text()
         date_th = date.replace(".","/")
         date_time , date_hour = date_th.split(" ")
-        print("title: ",title," description: ",description, " tarih: ",date_time ," Saat Dakika: ", type(date))
+
         # Data Set&Get
-        with open("Data/data.json","r",encoding="utf-8") as file:
+
+        with open("Data/data.json", "r", encoding="utf-8") as file:
                 data = json.load(file)
-        print("old: ",data)
-        new_data = {"title":title,"description": description, "time":data}
+        new_data = {"title": title, "description": description, "time": date}
+        
         data.append(new_data)
-        print("new: ",data)
+        with open("Data/data.json","w",encoding="utf-8") as file:
+             json.dump(data,file,indent=4,ensure_ascii=False) # İndent 4 almamızın sebebi JSON dosyasında daha düzenli şekilde gözükmesini sağlamaktır.
+        
+        # Listeyi Güncelleme 
+        self.todoList()
+        # Kaydedince başa dönme
+             
+        self.title_LineEdit.setText("")
+        self.description_LineEdit.setText("")
+        self.zaman_dateTimeEdit.setDateTime(QtCore.QDateTime.currentDateTime())
+
+    def todoList(self):
+        with open("Data/data.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        for i in reversed(range(self.verticalLayout.count())):
+            self.verticalLayout.itemAt(i).widget().setParent(None)
+        colors = ["#a6a6a6", "#737373"]
+        self.color = 0
+
+        for veri in data:
+            if self.color == 0:
+                self.color = 1
+                useColor = colors[self.color]
+            else:
+                self.color = 0
+                useColor = colors[self.color]
+
+            frame = QtWidgets.QFrame(self.scrollAreaWidgetContents_2)
+            frame.setMinimumSize(QtCore.QSize(624, 88))
+            frame.setMaximumSize(QtCore.QSize(624, 88))
+            frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+            frame.setFrameShadow(QtWidgets.QFrame.Raised)
+            frame.setStyleSheet(f"background-color:{useColor}")
+            frame.setObjectName("frame")
+
+            label_title = QtWidgets.QLabel(frame)
+            label_title.setGeometry(QtCore.QRect(40, 10, 521, 31))
+            label_title.setStyleSheet("QLabel{\n"
+                                      "color: #000;\n"
+                                      "font-family: Arial;\n"
+                                      "font-size: 22px;\n"
+                                      "font-style: normal;\n"
+                                      "font-weight: 700;\n"
+                                      "line-height: normal;\n"
+                                      "}")
+            label_title.setObjectName("label_4")
+
+            title = veri["title"]
+            label_title.setText(f"{title}")
+
+            checkBox = QtWidgets.QCheckBox(frame)
+            checkBox.setGeometry(QtCore.QRect(10, 30, 16, 21))
+            checkBox.setText("")
+            checkBox.setObjectName("checkBox")
+
+            label_desc = QtWidgets.QLabel(frame)
+            label_desc.setGeometry(QtCore.QRect(40, 40, 491, 31))
+            label_desc.setStyleSheet("QLabel{\n"
+                                     "color: #000;\n"
+                                     "font-family: Arial;\n"
+                                     "font-size: 15px;\n"
+                                     "font-style: normal;\n"
+                                     "font-weight: 400;\n"
+                                     "line-height: normal;\n"
+                                     "}")
+            label_desc.setObjectName("label_5")
+
+            description = veri["description"]
+            label_desc.setText(f"{description}")
+
+            pushButton_3 = QtWidgets.QPushButton(frame)
+            pushButton_3.setGeometry(QtCore.QRect(540, 30, 71, 41))
+            pushButton_3.setStyleSheet("QPushButton{\n"
+                                       "color: #FFF;\n"
+                                       "text-align: center;\n"
+                                       "font: 63 10pt \"Segoe UI\";\n"
+                                       "font-weight: 700;\n"
+                                       "background-color:rgb(2, 99, 255);\n"
+                                       "}")
+            pushButton_3.setObjectName("pushButton_3")
+            pushButton_3.setText("More")
+
+            self.verticalLayout.addWidget(frame)
+
 
 if __name__ == "__main__":
     import sys
