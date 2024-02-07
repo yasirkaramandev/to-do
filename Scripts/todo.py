712b18b4674e7293999b12d7d3777c5cb530f616
Yasir <yasirkaraman8@gmail.com>
2024-02-07 22:56:51 +0300

    Final

--- Scripts/todo.py
+++ Scripts/todo.py
@@ -2,7 +2,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import keyboard
 import json
-import datetime
+from datetime import datetime
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -116,7 +116,7 @@ class Ui_MainWindow(object):
 "}")
         self.delete_button.setObjectName("delete_button")
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.widget)
-        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(89, 258, 629, 657))
+        self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(89, 258, 670, 690))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
@@ -144,7 +144,6 @@ class Ui_MainWindow(object):
         self.todoList()
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Title (*)"))
         self.title_LineEdit.setPlaceholderText(_translate("MainWindow", "Title..."))
         self.label_2.setText(_translate("MainWindow", "Description (*)"))
@@ -157,27 +156,26 @@ class Ui_MainWindow(object):
         MainWindow.destroy(True,True)
 
     def addNewItem(self):
+        from random import randint
         if self.title_LineEdit.text() != "" or self.description_LineEdit.text() != "":
             # Data analyze
             title=self.title_LineEdit.text()
             description = self.description_LineEdit.text()
             date = self.zaman_dateTimeEdit.text()
             date_th = date.replace(".","/")
-            date_time , date_hour = date_th.split(" ")
+            _id = randint(1,10**10) - randint(1,10) + randint(1,2)
 
             # Data Set&Get
 
             with open("Data/data.json", "r", encoding="utf-8") as file:
                     data = json.load(file)
-            new_data = {"title": title, "description": description, "time": date}
+            new_data = {"title": title, "description": description, "time": date , "checked":False, "id":_id}
 
             data.append(new_data)
             with open("Data/data.json","w",encoding="utf-8") as file:
                  json.dump(data,file,indent=4,ensure_ascii=False) # İndent 4 almamızın sebebi JSON dosyasında daha düzenli şekilde gözükmesini sağlamaktır.
 
-            # Listeyi Güncelleme 
             self.todoList()
-            # Kaydedince başa dönme
 
             self.title_LineEdit.setText("")
             self.description_LineEdit.setText("")
@@ -185,6 +183,7 @@ class Ui_MainWindow(object):
         else: 
             QMessageBox = QtWidgets.QMessageBox()
             QMessageBox.warning(None,"Warning!","Please fill the fields.")
+    
     def todoList(self):
         with open("Data/data.json", "r", encoding="utf-8") as file:
             data = json.load(file)
@@ -192,6 +191,7 @@ class Ui_MainWindow(object):
             self.verticalLayout.itemAt(i).widget().setParent(None)
         colors = ["#a6a6a6", "#737373"]
         self.color = 0
+        index = 0
 
         for veri in data:
             if self.color == 0:
@@ -223,12 +223,15 @@ class Ui_MainWindow(object):
                                     """)
             label_title.setObjectName("label_4")
             title = veri["title"]
+            date = veri["time"]
             label_title.setText(f"{title}")
+            checked = veri["checked"]
             checkBox = QtWidgets.QCheckBox(frame)
+            checkBox.setChecked(checked)
             checkBox.setGeometry(QtCore.QRect(10, 30, 16, 21))
             checkBox.setText("")
             checkBox.setObjectName("checkBox")
-            checkBox.stateChanged.connect(lambda x,labelTitle = label_title , check_box = checkBox: self.dataControl(labelTitle,check_box))
+            checkBox.stateChanged.connect(lambda x,labelTitle = label_title , check_box = checkBox , data = veri, index = index: self.dataControl(labelTitle,check_box,data,index))
             label_desc = QtWidgets.QLabel(frame)
             label_desc.setGeometry(QtCore.QRect(40, 40, 491, 31))
             label_desc.setStyleSheet("QLabel{\n"
@@ -242,6 +245,8 @@ class Ui_MainWindow(object):
             label_desc.setObjectName("label_5")
             description = veri["description"]
             label_desc.setText(f"{description}")
+
+
             pushButton_3 = QtWidgets.QPushButton(frame)
             pushButton_3.setGeometry(QtCore.QRect(540, 30, 71, 41))
             pushButton_3.setStyleSheet("QPushButton{\n"
@@ -253,8 +258,40 @@ class Ui_MainWindow(object):
                                        "}")
             pushButton_3.setObjectName("pushButton_3")
             pushButton_3.setText("More")
+            pushButton_3.clicked.connect(lambda _, title=title, description=veri["description"],due_date = date, data = veri: self.showMoreInfoDialog(title, description,due_date,data))
+            if checkBox.isChecked():
+                label_title.setStyleSheet("""
+                                    QLabel{
+                                      color: red;
+                                      font-family: Arial;
+                                      font-size: 22px;
+                                      font-style: normal;
+                                      font-weight: 700;
+                                      line-height: normal;
+                                      text-decoration: line-through;
+                                      }
+                                    """)
+            else: 
+                label_title.setStyleSheet("""
+                                    QLabel{
+                                      color: #000;
+                                      font-family: Arial;
+                                      font-size: 22px;
+                                      font-style: normal;
+                                      font-weight: 700;
+                                      line-height: normal;
+                                      }
+                                    """)
             self.verticalLayout.addWidget(frame)
-    def dataControl(self,labelTitle,check_box):
+            index += 1
+
+        
+    
+    def dataControl(self,labelTitle,check_box,data,index):
+        with open("Data/data.json","r",encoding="utf-8") as file:
+            data = json.load(file)
+        data[index]["checked"] = check_box.isChecked()
+       
         if check_box.isChecked():
             labelTitle.setStyleSheet("""
                                     QLabel{
@@ -276,9 +313,14 @@ class Ui_MainWindow(object):
                                       font-style: normal;
                                       font-weight: 700;
                                       line-height: normal;
-                                      text-decoration: line-through;
                                       }
                                     """)
+        # data.json dosyasına yeni verileri yaz
+        with open("Data/data.json","w",encoding="utf-8") as file:
+            json.dump(data,file,indent=4)
+    
+
+    
     def deleteSelected(self):
         delete_indexes = []
 
@@ -297,6 +339,206 @@ class Ui_MainWindow(object):
         with open("Data/data.json","w",encoding="utf-8") as file:
             json.dump(data,file,ensure_ascii=False,indent=4)
         self.todoList()
+
+    def showMoreInfoDialog(self, title, description,due_date,data):
+        Dialog = QtWidgets.QDialog(MainWindow)
+        
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(677, 588)
+        Dialog.setStyleSheet("""QDialog{
+background-color: rgb(115, 115, 115);
+background-image: url();
+}
+                             """)
+        Dialog.setWindowTitle(title)
+        label = QtWidgets.QLabel(Dialog)
+        label.setGeometry(QtCore.QRect(40, 70, 211, 51))
+        label.setStyleSheet("color: #FFF;\n"
+"font-family: Inter;\n"
+"font-size: 36px;\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"line-height: normal;background-image: url();")
+        label.setObjectName("label")
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(40, 130, 561, 51))
+        self.lineEdit.setStyleSheet("QLineEdit{\n"
+"background-color: rgb(195, 195, 195);\n"
+"border:none;\n"
+"border-radius:12px;\n"
+"color: #fff;\n"
+"font-family: Arial;\n"
+"font-size: 22px;\n"
+"font-style: normal;\n"
+"font-weight: 400;\n"
+"line-height: normal;\n"
+"padding:8px;background-image: url();\n"
+"}")
+        self.lineEdit.setObjectName("lineEdit")
+        label_2 = QtWidgets.QLabel(Dialog)
+        label_2.setGeometry(QtCore.QRect(40, 190, 211, 51))
+        label_2.setStyleSheet("color: #FFF;\n"
+"font-family: Inter;\n"
+"font-size: 36px;\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"line-height: normal;background-image: url();")
+        label_2.setObjectName("label_2")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(40, 250, 561, 51))
+        self.lineEdit_2.setStyleSheet("QLineEdit{\n"
+"background-color: rgb(195, 195, 195);\n"
+"border:none;\n"
+"border-radius:12px;\n"
+"color: #fff;\n"
+"font-family: Arial;\n"
+"font-size: 22px;\n"
+"font-style: normal;\n"
+"font-weight: 400;\n"
+"line-height: normal;\n"
+"padding:8px;background-image: url();\n"
+"}")
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        label_3 = QtWidgets.QLabel(Dialog)
+        label_3.setGeometry(QtCore.QRect(40, 310, 211, 51))
+        label_3.setStyleSheet("color: #FFF;\n"
+"font-family: Inter;\n"
+"font-size: 36px;\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"line-height: normal;background-image: url();")
+        label_3.setObjectName("label_3")
+        horizontalLayoutWidget = QtWidgets.QWidget(Dialog)
+        horizontalLayoutWidget.setGeometry(QtCore.QRect(40, 360, 301, 51))
+        horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        horizontalLayoutWidget.setStyleSheet("background-image: url();")
+        horizontalLayout = QtWidgets.QHBoxLayout(horizontalLayoutWidget)
+        horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        horizontalLayout.setObjectName("horizontalLayout")
+        label_4 = QtWidgets.QLabel(horizontalLayoutWidget)
+        label_4.setStyleSheet("color: #FFF;\n"
+"font-family: Inter;\n"
+"font-size: 24px;\n"
+"font-style: normal;\n"
+"font-weight: 500;\n"
+"line-height: normal;background-image: url();")
+        label_4.setObjectName("label_4")
+        horizontalLayout.addWidget(label_4)
+        label_5 = QtWidgets.QLabel(horizontalLayoutWidget)
+        label_5.setStyleSheet("color: #FFF;\n"
+"font-family: Inter;\n"
+"font-size: 24px;\n"
+"font-style: normal;\n"
+"font-weight: 500;\n"
+"line-height: normal;background-image: url();")
+        label_5.setObjectName("label_5")
+        horizontalLayout.addWidget(label_5)
+        label_6 = QtWidgets.QLabel(horizontalLayoutWidget)
+        label_6.setStyleSheet("color: #FFF;\n"
+"font-family: Inter;\n"
+"font-size: 24px;\n"
+"font-style: normal;\n"
+"font-weight: 500;\n"
+"line-height: normal;background-image: url();")
+        label_6.setObjectName("label_6")
+        horizontalLayout.addWidget(label_6)
+        pushButton = QtWidgets.QPushButton(Dialog)
+        pushButton.setGeometry(QtCore.QRect(190, 510, 121, 51))
+        pushButton.setStyleSheet("QPushButton{\n"
+"background-color: rgb(0, 255, 128);\n"
+"color: #FFF;\n"
+"text-align: center;\n"
+"font-family: Inter;\n"
+"font-size: 20px;\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"line-height: normal;\n"
+"border-radius:12px;\nbackground-image: url();"
+"}\n"
+"QPushButton:hover{\n"
+"background-color: rgb(0, 255, 80);\n"
+"}\n"
+"QPushButton:pressed{background-color: rgb(0, 255, 0);}")
+        pushButton.setObjectName("pushButton")
+        pushButton_2 = QtWidgets.QPushButton(Dialog)
+        pushButton_2.setGeometry(QtCore.QRect(350, 510, 121, 51))
+        pushButton_2.setStyleSheet("QPushButton{\n"
+"background-color: rgb(255, 0, 0);\n"
+"color: #FFF;\n"
+"text-align: center;\n"
+"font-family: Inter;\n"
+"font-size: 20px;\n"
+"font-style: normal;\n"
+"font-weight: 600;\n"
+"line-height: normal;\n"
+"border-radius:12px;background-image: url();\n"
+"}\n"
+"QPushButton:hover{\n"
+"background-color: rgb(255, 0, 80);\n"
+"}\n"
+"QPushButton:pressed{background-color: rgb(255, 0, 128);}")
+        pushButton_2.setObjectName("pushButton_2")
+
+        label.setText("Title")
+        self.lineEdit.setPlaceholderText(f"{title}")
+        label_2.setText("Description")
+        self.lineEdit_2.setPlaceholderText(f"{description}")
+        label_3.setText("Remaining")
+        current_datetime = datetime.now()
+
+        due_datetime = datetime.strptime(due_date, "%d.%m.%Y %H:%M")
+
+        time_difference = due_datetime - current_datetime
+
+        if time_difference.total_seconds() > 0:
+            remaining_days = time_difference.days
+            remaining_hours, remaining_seconds = divmod(time_difference.seconds, 3600)
+            remaining_minutes = remaining_seconds // 60
+        else:
+            remaining_days, remaining_hours, remaining_minutes = 0, 0, 0
+
+        label_4.setText(f"{remaining_days} Days")
+        label_5.setText(f"{remaining_hours} Hours")
+        label_6.setText(f"{remaining_minutes} Min")
+        pushButton.setText("Save")
+        pushButton_2.setText("Delete")
+        pushButton_2.clicked.connect(lambda _, titleA=data["id"]:self.deleteItem(titleA))
+        pushButton_2.clicked.connect(Dialog.accept)
+        pushButton.clicked.connect(lambda x, _id=data["id"], title=self.lineEdit.text(), description=self.lineEdit_2.text(): self.saveData(_id, title, description))
+        Dialog.exec_()
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+    def deneme(self,veri):
+        print(veri)
+    def deleteItem(self, title):
+        with open("Data/data.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        index_to_delete = next((index for index, item in enumerate(data) if item["id"] == title), None)
+
+        if index_to_delete is not None:
+            del data[index_to_delete]
+
+            with open("Data/data.json", "w", encoding="utf-8") as file:
+                json.dump(data, file, indent=2)
+
+            self.todoList()
+    def saveData(self, _id, new_title, new_description):
+        with open("Data/data.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
+        
+        new_title = self.lineEdit.text()
+        new_description = self.lineEdit_2.text()
+        index = 0
+        for item in data:
+            if item["id"] == _id:
+                item["title"] = new_title
+                item["description"] = new_description
+                with open("Data/data.json", "w", encoding="utf-8") as file:
+                    json.dump(data, file, indent=4)
+                break
+            index += 1
+        self.todoList()
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -305,4 +547,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-    #text-decoration: line-through;
